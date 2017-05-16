04503c9641cf97db94f2055334b50580bdae85cf
Elvis <orest.kupchak@gmail.com>
2017-05-16 15:44:31 +0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,10 @@
+ 
+
+let person = 'Orest';
+let time = '12.15';
+let place = 'Empire State Building'
+let event = prompt("What is on your schedule for today ?")
+
+
+console.log(`${person} is having ${event} at ${time} at the ${place} today !`);
+
